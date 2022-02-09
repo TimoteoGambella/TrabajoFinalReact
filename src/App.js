4c2d1daa418211535fd93cea060e46e63b0b8727
TimoteoGambella <timi.gambella@hotmail.com>
2022-02-09 14:35:28 -0300

    componente ItemCount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ListContainer/ItemList';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <NavBar />
         <ItemListContainer text="HOLA COMO ESTAN ME LLAMO TIMOTEO GAMBELLA Y SOY PROGRAMADOR" title="TIMOTEO" />
         <ItemListContainer text="ESTE ES EL CURSO DE REACT JS Y ESTA ES UNA PRUEBA DE COMPONENTES" title="PROGRAMACION" />
+        <ItemCount numberStock="5" />
     </React.Fragment>
 
   );
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import "./ItemCount.css"
+import signoMas from "./mas.png"
+import signoMenos from "./menos.png"
+
+export default function ItemCount(numberStock){
+    const [contador, setContador]=useState(1)
+    const stock = 5
+    console.log(stock)
+
+    useEffect(()=>{
+        if(contador%2==0){
+            document.querySelector('.ItemCount-container').style.border = '3px solid lightblue';
+        }
+        if(contador%2!=0){
+            document.querySelector('.ItemCount-container').style.border = '3px solid red';
+        }
+    },[contador])
+
+    const ClickSuma = ()=>{
+        if(contador<stock){
+            setContador(contador+1)
+        }
+    }
+    const ClickResta = ()=>{
+        if(contador>1){
+            setContador(contador-1)
+        }
+    }
+
+    return(
+        <div className='ItemCount-container'>
+            <img src={signoMas} className='ItemCount-img' onClick={ClickSuma}></img>
+            <p className='ItemCount-p'>{contador}</p>
+            <img src={signoMenos} className='ItemCount-img' onClick={ClickResta}></img>
+        </div>
+    )
+}
