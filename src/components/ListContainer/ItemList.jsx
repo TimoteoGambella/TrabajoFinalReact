4553c34e7857424ca75fa98349516e9ec5058c73
TimoteoGambella <timi.gambella@hotmail.com>
2022-02-18 19:30:02 -0300

    useEffect en itemlist para renderizar mejor

--- src/components/ListContainer/ItemList.jsx
+++ src/components/ListContainer/ItemList.jsx
@@ -1,25 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import "./ItemList.css"
 import baseDeDatos from "../ListContainer/productos.json"
 import Items from '../Items/Items';
 
 export default function ItemListContainer(){
 
-
     const [productos, setProductos] = useState([])
-
-    const prodPromise = new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            setProductos(baseDeDatos)
-            setInfo(false)
-            resolve(true)
-        }, 2000);
-    })
-    prodPromise.then(()=>{console.log("COMPLETADO")})
-    prodPromise.catch(()=>{console.log("ERROR 404")})
 
     const [info, setInfo] = useState(true)
     const mensaje = "Cargando..."
+
+    useEffect(()=>{
+        const prodPromise = new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                setProductos(baseDeDatos)
+                setInfo(false)
+                resolve(true)
+            }, 2000);
+        })
+        prodPromise.then(()=>{console.log("COMPLETADO")})
+        prodPromise.catch(()=>{console.log("ERROR 404")})
+    },[])
 
         return(
             <>{info ? (<p className="ItemTitle-center">{mensaje}</p>
